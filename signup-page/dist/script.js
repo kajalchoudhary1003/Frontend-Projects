a42561e7b66aa2c4b5a35e1b7c8e93d802091986
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-07-11 23:34:08 +0530

    **

--- signup-page/dist/script.js
+++ signup-page/dist/script.js
@@ -1,5 +1,14 @@
 const toggle = document.getElementById('toggle');
 const password = document.getElementById('password');
+const confirmpassword=document.getElementById('cnfpwd');
+const form = document.getElementById('form');
+const display = document.getElementById('display');
+
+form.removeChild(display);
+
+password.addEventListener('input', ()=>{
+    form.appendChild(display);
+});
 
 toggle.addEventListener('click', function(){
     const type= password.getAttribute('type') === 'password' ? 'text' : 'password';
@@ -7,3 +16,20 @@ toggle.addEventListener('click', function(){
 // toggle below is a different function
     this.classList.toggle('fa-eye');
 });
+
+
+
+
+
+validate = () => {
+    if(password.value != confirmpassword.value){
+        confirmpassword.setCustomValidity("Passwords don't Match");
+    }else{
+        confirmpassword.setCustomValidity('');
+    }
+}
+
+password.onchange = validate;
+confirmpassword.onkeyup =validate;
+
+
